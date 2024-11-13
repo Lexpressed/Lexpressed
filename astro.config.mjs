c52e1ef2bfe324a5162cb017d7c6f4086bc0a1cf
Nicolas Posa <nicoposa57@gmail.com>
2024-11-13 18:18:02 -0300

    vercel settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), mdx()],
   renderers: ['@astrojs/renderer-preact']
 });
